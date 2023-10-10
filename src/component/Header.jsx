01ff1bd5f73459961d39967ca998fecabb8ef108
IT21150098 <it21150098@my.sliit.lk>
2023-10-10 09:46:07 +0530

    home icone change

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,4 +1,4 @@
-import {  FaSignOutAlt } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logout, reset } from "../features/auth/authSlice";
@@ -10,42 +10,43 @@ function Header() {
   const onLogout = () => {
     dispatch(logout());
     dispatch(reset());
-    window.location.href ="/"
+    window.location.href = "/";
   };
 
   return (
     <>
-    {
-      user ? 
-      <header className="header">
-      <div className="logo">
-        <Link to="/">Todo </Link>
-      </div>
-      <ul>
-          <>
-          <li>
-              <button className="btn" >
-                {user.name}
-              </button>
-            </li>
-            <li>
-              <button className="btn" >
-                Add
-              </button>
-            </li>
-            <li>
-              <button className="btn" onClick={onLogout}>
-                <FaSignOutAlt /> Logout
-              </button>
-            </li>
-          </>
-      </ul>
-    </header>
-    :<></>
-
-    }
+      {user ? (
+        <header className="header">
+          <div className="logo">
+            <Link to="/">Todo </Link>
+          </div>
+          <ul>
+            <>
+              <li>
+                <button className="btn">{user.name}</button>
+              </li>
+              <li>
+                <button className="btn">
+                  <Link
+                    to="/todo/add"
+                    style={{ textDecoration: "none", color: "white" }}
+                  >
+                    Add
+                  </Link>
+                </button>
+              </li>
+              <li>
+                <button className="btn" onClick={onLogout}>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          </ul>
+        </header>
+      ) : (
+        <></>
+      )}
     </>
-    
   );
 }
 
